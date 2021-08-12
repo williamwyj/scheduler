4c969cb55f39f51aaf874a590970e751a60b1c10
William Wang <williamwyj1@gmail.com>
2021-08-11 21:01:57 -0700

    add jest test for reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,54 +1,9 @@
 import React, { useState, useEffect, useReducer } from "react";
 import axios from 'axios';
 
+import reducer, {SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW, SET_SPOTS} from "reducers/application";
+
 export default function useApplicationData() {
-  
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  const SET_SPOTS = "SET_SPOTS"
-  
-  function reducer(state, action) {
-    
-    switch (action.type) {
-      case SET_DAY:
-        const day = action.day
-        return {...state, day}
-      case SET_APPLICATION_DATA: {
-        const days = action.days;
-        const appointments = action.appointments;
-        const interviewers = action.interviewers;
-        return {...state, days, appointments, interviewers}
-      }
-      case SET_INTERVIEW: {
-        const appointments = action.appointments
-        return {...state, appointments}
-      }
-      case SET_SPOTS: {
-        const day = state.days.filter(element => element.name === state.day)
-          let spot = day[0].appointments.length;
-          const interviews = day[0].appointments.map(id => state.appointments[id])
-          for (const interview of interviews) {
-            if (interview.interview) {
-              spot--;
-            }
-          }
-          const days = state.days.map(day => {
-            return {...day}
-          })
-          for (const day of days) {
-            if (day.name === state.day) {
-              day.spots = spot
-            }
-          }
-        return {...state, days}
-      }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type : ${action.type}`
-      );
-    }
-  }
 
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
